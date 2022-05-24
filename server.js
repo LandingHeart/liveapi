5613474f3211e10ee4d301fce4bf63c48c765410
LandingHeart <shinan.liu@yahoo.com>
2022-05-24 15:56:17 -0400

    save

--- server.js
+++ server.js
@@ -4,24 +4,22 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = 8080;
 const db = mongoose.connection;
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./configs/.env") });
+
+const connectToDB = require("./configs/connectToDB");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api", require("./src/index"));
 
-const connectionURL = `mongodb+srv://admin:${process.env.DB_PASSWORD}@defaultcluster.bzvjf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-mongoose.connect(connectionURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-app.listen(PORT, () => {
-  console.log(`Server Running on port ${PORT}`);
-});
+connectToDB();
 
 db.on("error", console.error.bind(console, "connection error: "));
+
 db.once("open", function () {
   console.log("Connected to database!!!!!");
+  app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
+  });
 });
